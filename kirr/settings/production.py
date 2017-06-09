bca4f5ea813933bf3c7e04ead3672c4f054f6f72
rathee95 <rathee95@gmail.com>
2017-06-09 22:15:13 +0530

    debug true

--- kirr/settings/production.py
+++ kirr/settings/production.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = '1_36z!-d34*dz#$_&v1g4fz4mzb=$px#19n-#9vbo48+tfm$&c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 #this is the production settings file
 
 ALLOWED_HOSTS = ['justshortit.herokuapp.com', 'www.justshortit.herokuapp.com', 'https://justshortit.herokuapp.com','http://justshortit.herokuapp.com' ]######################33
@@ -59,7 +59,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'kirr.urls'
 ROOT_HOSTCONF = 'kirr.hosts'
-DEFAULT_HOST = 'www'
+DEFAULT_HOST = 'www' 
 DEFAULT_REDIRECT_URL= "/"#???ye krke dekhu? han ekmin
 #########agar yha justshortit.herokuapp.com dalu to dikhata hai too many redirects, agar blankchodu to 
 PARENT_HOST = 'https://justshortit.herokuapp.com/'###############333333ye dikkat hai ab anytihng?
