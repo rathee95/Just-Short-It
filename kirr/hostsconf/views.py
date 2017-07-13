b1972a1e181cd3a237bbaa6edf1a7996070fc26a
rathee95 <rathee95@gmail.com>
2017-07-13 18:23:02 +0530

    defaulturl11

--- kirr/hostsconf/views.py
+++ kirr/hostsconf/views.py
@@ -5,7 +5,8 @@ from django.conf import settings
 DEFAULT_REDIRECT_URL = getattr(settings,"DEFAULT_REDIRECT_URL","myshortner.herokuapp.com") 
 
 def wildcard_redirect(request,path= None):
-	new_url = DEFAULT_REDIRECT_URL
-	# if path is not None:
-	# 	new_url == DEFAULT_REDIRECT_URL + "/" + path
-	return HttpResponseRedirect(new_url) 
+		pass
+	# new_url = DEFAULT_REDIRECT_URL
+	# 	# if path is not None:
+	# 	# 	new_url == DEFAULT_REDIRECT_URL + "/" + path
+	# return HttpResponseRedirect(new_url) 
