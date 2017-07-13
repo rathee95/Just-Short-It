42fb8a7981a9923a30722ce321cd42b08fced579
rathee95 <rathee95@gmail.com>
2017-07-13 18:33:16 +0530

    defaulturl11

--- kirr/urls.py
+++ kirr/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url( r'^$',HomeView.as_view() ),
     # url(r'^a/(?P<shortcode>[\w-]+)/$', kirr_redirect_view),
-    url(r'^(?P<shortcode>[\w-]+)/$', URLRedirectView.as_view(),name='scode'),
+    # url(r'^(?P<shortcode>[\w-]+)/$', URLRedirectView.as_view(),name='scode'),
 
 ]
