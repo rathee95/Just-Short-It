726f7e1a6ba6a9f4406fc1ff7133de5acd4db87e
rathee95 <rathee95@gmail.com>
2017-07-13 18:42:20 +0530

    defaulturl11

--- kirr/hostsconf/urls.py
+++ kirr/hostsconf/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
-from .views import wildcard_redirect
+# from django.conf.urls import url
+# from .views import wildcard_redirect
 
-urlpatterns = [
-	url(r'^(?P<path>.*)',wildcard_redirect),
-]
+# urlpatterns = [
+# 	url(r'^(?P<path>.*)',wildcard_redirect),
+# ]
