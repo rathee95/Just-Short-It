75bbdc559d0970a42500da63d24b2b463f2a7883
rathee95 <rathee95@gmail.com>
2017-07-13 17:11:29 +0530

    deploy try

--- kirr/settings/production.py
+++ kirr/settings/production.py
@@ -138,3 +138,15 @@ STATIC_URL = '/static/'
 
 SHORTCODE_MIN = 6 
 SHORTCODE_MAX = 6 
+
+
+CORS_REPLACE_HTTPS_REFERER      = True
+HOST_SCHEME                     = "https://"
+SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT             = True
+SESSION_COOKIE_SECURE           = True
+CSRF_COOKIE_SECURE              = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+SECURE_HSTS_SECONDS             = 1000000
+SECURE_FRAME_DENY               = True
+
